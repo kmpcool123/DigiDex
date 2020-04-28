ff9881e1b923bba33ec9b4103c3671f62abc0df7
Jonathan Ingersoll <jonathan.ingersoll@gmail.com>
2020-04-28 11:43:42 -0400

    Added properties to Deck class - Added models DeckCreate, DeckDetail, DeckEdit, and DeckListItem

---
+++ DigiDex.Models/Deck.Models/DeckListItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigiDex.Models
+{
+    public class DeckListItem
+    {
+        public int DeckId { get; set; }
+        public string DeckTitle { get; set; }
+        public int CategoryId { get; set; }
+        [Display(Name = "Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+    }
+}
