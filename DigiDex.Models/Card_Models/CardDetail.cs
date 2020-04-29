1ea4a113ae6114c50e20c3af3d0270b70714cb4e
kmpcool123 <khalil.perkins98@gmail.com>
2020-04-29 10:33:52 -0400

    changes

--- DigiDex.Models/Card_Models/CardDetail.cs
+++ DigiDex.Models/Card_Models/CardDetail.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DigiDex.Models.CardModels
+namespace DigiDex.Models.Card_Models
 {
     public class CardDetail
     {
