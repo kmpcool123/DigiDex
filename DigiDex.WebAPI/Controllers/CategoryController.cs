ce216061060fafac9a624c41e2984086a3921701
Erin Croteau <erin.croteau@gmail.com>
2020-04-29 15:16:18 -0400

    E added CategoryController and GetCategoryByTitle service

---
+++ DigiDex.WebAPI/Controllers/CategoryController.cs
@@ -0,0 +1,71 @@
+﻿using DigiDex.Models.Category_Models;
+using DigiDex.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DigiDex.WebAPI.Controllers
+{
+    public class CategoryController : ApiController
+    {
+        private CategoryService CreateCategoryService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var categoryService = new CategoryService(userId);
+            return categoryService;
+        }
+        public IHttpActionResult GetAll()
+        {
+            CategoryService categoryService = CreateCategoryService();
+            var categories = categoryService.GetCategories();
+            return Ok(categories);
+        }   
+
+        public IHttpActionResult GetCategoryById(int id)
+        {
+            CategoryService categoryService = CreateCategoryService();
+            var category = categoryService.GetCategoryById(id);
+            return Ok(category);
+        }
+
+    public IHttpActionResult GetCategoryByTitle(string categoryTitle)
+        {
+            CategoryService categoryService = CreateCategoryService();
+            var category = categoryService.GetCategoryByTitle(categoryTitle);
+            return Ok(category);
+        }
+
+        public IHttpActionResult Post(CategoryCreate category)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var service = CreateCategoryService();
+            if (!service.CreateCategory(category))
+                return InternalServerError();
+            return Ok();
+        }
+
+
+        public IHttpActionResult Put(CategoryEdit category)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var service = CreateCategoryService();
+            if (!service.UpdateCategory(category))
+                return InternalServerError();
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreateCategoryService();
+            if (!service.DeleteCategory(id))
+                return InternalServerError();
+            return Ok();
+        }
+    }
+}
